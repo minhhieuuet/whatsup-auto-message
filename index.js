d3f9af34f401b5da7d3469ed407eefffa48860b1
Sotatek-Hieu.Do <hieu.do@sotatek.com>
2021-07-15 12:05:05 +0700

    Whatsup auto message

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ const puppeteer = require('puppeteer');
   await page.goto('https://web.whatsapp.com');
   await page.waitForSelector('button[aria-label="Voice message"]');
   
-  await page.waitFor(5000);
+  const waitTime = 5000; //Thoi gian doi sau khi login (ms)
+  const message = "Tran Hoang Anh lock R3-08 (R3-03) tang thap" // Tin nhan khong dau;
+  const delay = 200; //Thoi gian delay (ms)
+
+  await page.waitFor(waitTime);
+
+
   setInterval(async () => {
-    "Tran Hoang Anh lock R3-08 (R3-03) tang thap".split("").forEach(async (char) => {
+    message.split("").forEach(async (char) => {
       await page.keyboard.press(char);
     })
     await page.keyboard.press('Enter');
     console.log("sent");
-  }, 200);
+  }, delay);
   // await browser.close();
 })();
