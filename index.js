fd9d6b57ef61eea673098adee1e2838b49af82b7
Sotatek-Hieu.Do <hieu.do@sotatek.com>
2021-07-17 21:01:24 +0700

    Whatsup auto message

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://web.whatsapp.com');
+  await page.goto("https://web.whatsapp.com");
   await page.waitForSelector('button[aria-label="Voice message"]');
-  
+
+  //Copy text can spam truoc khi su dung
   const waitTime = 5000; //Thoi gian doi sau khi login (ms)
-  const message = "Tran Hoang Anh lock R3-08 (R3-03) tang thap" // Tin nhan khong dau;
   const delay = 200; //Thoi gian delay (ms)
 
-  await page.waitFor(waitTime);
+  await page.waitFor(5000);
 
-
-  setInterval(async () => {
-    message.split("").forEach(async (char) => {
-      await page.keyboard.press(char);
-    })
-    await page.keyboard.press('Enter');
+  let interval;
+  interval = setInterval(async () => {
+    await page.keyboard.down("Control");
+    await page.keyboard.press("V");
+    await page.keyboard.up("Control");
+    await page.keyboard.press("Enter");
     console.log("sent");
   }, delay);
   // await browser.close();
