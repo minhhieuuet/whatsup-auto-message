3df8f620c5123326668a0d83845957f4351b029d
Sotatek-Hieu.Do <hieu.do@sotatek.com>
2021-07-17 21:17:28 +0700

    Whatsup auto message

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const puppeteer = require("puppeteer");
-
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://web.whatsapp.com");
-  await page.waitForSelector('button[aria-label="Voice message"]');
-
+  // await page.waitForSelector('button[aria-label="Voice message"]');
+  await page.waitForNavigation();
   //Copy text can spam truoc khi su dung
-  const waitTime = 5000; //Thoi gian doi sau khi login (ms)
   const delay = 200; //Thoi gian delay (ms)
 
-  await page.waitFor(waitTime);
-
-  let interval;
-  interval = setInterval(async () => {
-    await page.keyboard.down("Control");
-    await page.keyboard.press("V");
-    await page.keyboard.up("Control");
-    await page.keyboard.press("Enter");
-    console.log("sent");
-  }, delay);
+  console.log("Page loaded");
+  readline.question("Press 'start' to spamming : ", (command) => {
+    if (command == "start") {
+      setInterval(async () => {
+        await page.keyboard.down("Control");
+        await page.keyboard.press("V");
+        await page.keyboard.up("Control");
+        await page.keyboard.press("Enter");
+        console.log("sent");
+      }, delay);
+      readline.close();
+    }
+  });
   // await browser.close();
 })();
